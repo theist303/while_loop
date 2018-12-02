fc64ccfc2ecaa321fa0e1afbeb39a61a6d3dccbc
Mashood <45217431+theist303@users.noreply.github.com>
2018-12-02 14:19:02 +0100

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,12 @@
+#WHILE LOOP ASSIGNMENT
+
+x = input ('enter any figure') 
+while (x<=100):
+    print(x)
+    x+=1
+    if(x%3==0):
+        print(x,'fizz')
+    elif(x%5==0):
+            print(x,'buzz')
+    if(x%3==0 and x%5==0):
+            print(x,'fizzbuzz')
